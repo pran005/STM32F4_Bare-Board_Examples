61731b71dad5bd0d969f2208a91958d9b3808664
Pranjal Shrivastava <52209005+pran005@users.noreply.github.com>
2019-08-01 22:40:44 +0530

    Update main.cpp

--- ADC/main.cpp
+++ ADC/main.cpp
@@ -7,15 +7,15 @@
 int main(void)
 {	
 	  SystemInit() ; 
-  	RCC->APB2ENR |=  RCC_APB2ENR_ADC1EN | RCC_APB2ENR_IOPAEN; 
-    init_lcd() ;    
-	  GPIOA -> CRL &= ~ (1<<2) ; 
-		ADC1 -> CR2 |= (1<<0) | (1<<1) ;
-		ADC1 -> SMPR1 |= (1<<18) | (1<19)  ; 
-	  ADC1 -> SQR3 |= (0<<0) ; 
-    ADC1 -> CR1 |= (1<<5) ; 
-	  NVIC_EnableIRQ(ADC1_IRQn) ;
-		ADC1 -> CR2 |= (1<<0) ; 
+  	  RCC->APB2ENR |=  RCC_APB2ENR_ADC1EN | RCC_APB2ENR_IOPAEN; //enable clocks
+    	  init_lcd() ;    
+	  GPIOA -> CRL &= ~ (1<<2) ;		 // analog in mode 
+	  ADC1 -> CR2 |= (1<<0) | (1<<1) ;	// power ON ADC | Continous mode
+	  ADC1 -> SMPR1 |= (1<<18) | (1<19)  ; // setup sample speed
+	  ADC1 -> SQR3 |= (0<<0) ; 	      //setup sequence number
+    	  ADC1 -> CR1 |= (1<<5) ; 	     //enable ADC interrupts
+	  NVIC_EnableIRQ(ADC1_IRQn) ;		
+	  ADC1 -> CR2 |= (1<<0) ;	   // Trigger Conversion		
 
 }
 
@@ -24,12 +24,12 @@ extern "C" {
 void ADC1_IRQHandler(void) 
 {
 		uint32_t data = ADC1 ->DR ; 
-	  clr_lcd() ; 
-		int moisture = ((4095 - data)*100/4095);  
+	  	clr_lcd() ; 
+		int moisture = ((4095 - data)*100/4095);  //scale moisture from moisture sensor
 		lcd((char*)"MOISTURE :") ; 
 		lcd(moisture) ;
 		lcd((char*)"%") ; 
-	  delayms(1000) ; 
+	  	delayms(1000) ; 
 		
 
 }
